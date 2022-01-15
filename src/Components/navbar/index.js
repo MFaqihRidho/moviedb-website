76b4121f759dd684276bc08c38e6a1bc9bfaa81b
MFaqihRidho <faqih.fr5@gmail.com>
2022-01-15 07:11:55 +0700

    modfying navbar

--- src/Components/navbar/index.js
+++ src/Components/navbar/index.js
@@ -58,12 +58,12 @@ export default function Navbar() {
                     </div>
                     <input
                         type="text"
-                        className="z-0 px-2 w-3/5 sm:w-full py-2 bg-gray-500 rounded-lg shadow md:px-10 focus:outline-none focus:ring focus:ring-sky-900/60"
+                        className="z-0 px-2 w-1/2 sm:w-full py-2 bg-gray-500 rounded-lg shadow md:px-10 focus:outline-none focus:ring focus:ring-sky-900/60"
                         placeholder="Search Movie"
                         onChange={(e) => changeHandle(e)}
                         value={data}
                     />
-                    <div class="absolute top-1.5 right-2">
+                    <div class="absolute top-1.5 right-3">
                         <button
                             onClick={search}
                             id="search"
