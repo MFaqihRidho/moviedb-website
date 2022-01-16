b87d3aa4fcb0da0d6bbc11077111e822458115fa
MFaqihRidho <faqih.fr5@gmail.com>
2022-01-16 10:25:22 +0700

    adding params

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Navbar from "../../Components/navbar";
 // import Result from "../../Components/result";
+import { useParams } from "react-router-dom";
 import TrendingMovies from "../../Components/Trending Card";
 import Footer from "../../Components/footer";
 
 export default function Search() {
+    let params = useParams();
     return (
         <div className="min-h-screen bg-black -z-50 font-poppins">
             <Navbar></Navbar>
             {/* <Result></Result> */}
             <TrendingMovies></TrendingMovies>
-            <h1>Spiderman</h1>
+            <h1 className="text-white text-4xl">{params.keyword}</h1>
             <Footer></Footer>
         </div>
     );
