1ddd00b4244959aa0e09d50e90b402a34d069151
MFaqihRidho <faqih.fr5@gmail.com>
2022-01-16 10:14:40 +0700

    trying solve bug

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "../../Components/navbar";
-import Result from "../../Components/result";
+// import Result from "../../Components/result";
 import Footer from "../../Components/footer";
 
 export default function Search() {
     return (
         <div className="min-h-screen bg-black -z-50 font-poppins">
             <Navbar></Navbar>
-            <Result></Result>
+            {/* <Result></Result> */}
+            <h1>Spiderman</h1>
             <Footer></Footer>
         </div>
     );
